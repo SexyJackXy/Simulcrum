89c977c2719391d47cdb30b0b4de131675f2c10a
SexyJackXy <tjachmann26@gmail.com>
2023-12-28 02:04:41 +0100

    Create: Added Code Behinde - Created Code Behind filde in new Folder `Code` - Fixed some Css Problems

---
+++ MauiApp2/Code/CreateBackup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace simulcrum
+{
+	public static class CreateBackup
+	{
+		public static void destbtn_Click()
+		{
+		
+		}
+	}
+}
